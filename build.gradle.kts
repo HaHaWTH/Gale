17d43aaeaf457daf1d5ccdcc23b63460bf5210fa
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-11-28 03:08:06 -0500

    [ci skip] Fix gale version fetcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import kotlin.io.path.deleteRecursively
 plugins {
     java
     `maven-publish`
-    id("io.papermc.paperweight.patcher") version "1.7.4"
+    id("io.papermc.paperweight.patcher") version "1.7.5"
 }
 
 allprojects {
